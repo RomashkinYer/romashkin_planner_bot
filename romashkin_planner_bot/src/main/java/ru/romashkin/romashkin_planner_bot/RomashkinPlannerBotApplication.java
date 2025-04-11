f97f61439216c0e93aa543e8a22eee77e98777b8
RomashkinYer <roma_erilov@mail.ru>
2025-04-12 02:28:52 +1000

    Initial commit

---
+++ romashkin_planner_bot/src/main/java/ru/romashkin/romashkin_planner_bot/RomashkinPlannerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.romashkin.romashkin_planner_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomashkinPlannerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomashkinPlannerBotApplication.class, args);
+	}
+
+}
---
+++ romashkin_planner_bot/src/test/java/ru/romashkin/romashkin_planner_bot/RomashkinPlannerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romashkin.romashkin_planner_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomashkinPlannerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
