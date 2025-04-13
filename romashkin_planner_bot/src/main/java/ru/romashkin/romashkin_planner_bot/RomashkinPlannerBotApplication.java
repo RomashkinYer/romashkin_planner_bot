58db15b21917c11c917023f30bb074b4c2fba8be
RomashkinYer <roma_erilov@mail.ru>
2025-04-13 23:34:36 +1000

    Иницилизировал бота

--- romashkin_planner_bot/src/main/java/ru/romashkin/romashkin_planner_bot/RomashkinPlannerBotApplication.java
+++ romashkin_planner_bot/src/main/java/ru/romashkin/romashkin_planner_bot/RomashkinPlannerBotApplication.java
@@ -1,13 +1,28 @@
 package ru.romashkin.romashkin_planner_bot;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.impl.TelegramBotClient;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class RomashkinPlannerBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RomashkinPlannerBotApplication.class, args);
+		SpringApplication.run(RomashkinPlannerBotApplication.class, args); //Запускает наше Спринг приложение
+
+		String token = "7889737617:AAEEosnOKjR7R7HUwAQGYIHp4MUkwxU6Zk8";
+		TelegramBot bot = new TelegramBot(token);
+		System.out.println("Бот запущен");
+
+
+
 	}
 
 }
---
+++ romashkin_planner_bot/src/main/java/ru/romashkin/romashkin_planner_bot/bot/RomashkinPlannerBot.java
@@ -0,0 +1,10 @@
+package ru.romashkin.romashkin_planner_bot.bot;
+
+import com.pengrad.telegrambot.TelegramBot;
+
+public class RomashkinPlannerBot  {
+
+
+
+
+}
